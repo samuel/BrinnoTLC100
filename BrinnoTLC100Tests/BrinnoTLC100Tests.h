3cb9c89dd83474302a3a1e6572de7c1f887327c9
Samuel Stauffer <samuel@descolada.com>
2015-01-22 20:12:17 -0800

    Auto-update for latest Xcode

--- BrinnoTLC100Tests/BrinnoTLC100Tests.h
+++ BrinnoTLC100Tests/BrinnoTLC100Tests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Samuel Stauffer. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface BrinnoTLC100Tests : SenTestCase
+@interface BrinnoTLC100Tests : XCTestCase
 
 @end
